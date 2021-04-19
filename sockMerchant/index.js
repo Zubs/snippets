283195702e66b70118e6e483f3348fe161203256
Zubs <zubairidrisaweda@gmail.com>
2021-04-20 00:54:40 +0100

    Another one done

---
+++ sockMerchant/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const getOccurence = (array, value) => {
+    let count = 0;
+    array.forEach((item) => { item === value ? count++ : count = count; });
+    return count;
+};
+const sockMerchant = (numberOfSocks, socks) => {
+    // Confirm input is correct
+    if (socks.length !== numberOfSocks) {
+        return false;
+    }
+    let answer = 0;
+    new Set(socks).forEach((item) => {
+        answer += (Math.floor(getOccurence(socks, item) / 2));
+    });
+    return answer;
+};
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
+console.log(sockMerchant(10, [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]));

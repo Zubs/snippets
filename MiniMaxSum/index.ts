5a65c138d2c7362e22ffe68392b94a8dc64d5b60
Zubs <zubairidrisaweda@gmail.com>
2021-04-21 23:51:10 +0100

    Done

---
+++ MiniMaxSum/index.ts
@@ -0,0 +1,11 @@
+const miniMaxSum = (arr: number[]): string => {
+	arr = arr.sort();
+
+	const output1: number = arr.reduce((total, num) => { return total + num; }) - arr[0];
+	const output2: number = arr.reduce((total, num) => { return total + num; }) - arr[arr.length - 1];
+	
+	return `${ output2 } ${ output1 }`;
+};
+
+console.log(miniMaxSum([1, 2, 3, 4, 5]));
+console.log(miniMaxSum([7, 69, 2, 221, 8974]))

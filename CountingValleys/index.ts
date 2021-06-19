e0aed8c009e9f100e6afe4e8f8056e47d716417c
Zubs <zubairidrisaweda@gmail.com>
2021-06-19 13:13:07 +0100

    ..

---
+++ CountingValleys/index.ts
@@ -0,0 +1,28 @@
+/**
+ * @param steps The number of steps on the hike
+ * @param path A string describing the path
+ * @returns { number } The number of valleys traversed
+ */
+function countingValleys(steps: number, path: string): number {
+    var valleysMoved = 0;
+    var level = 0;
+
+    const paths = path.split('');
+
+    paths.forEach((path) => {
+        if (path === 'D') {
+            if (level === 0) {
+                valleysMoved++
+            }
+            level--
+        } else if (path === 'U') {
+            level++
+        }
+    });
+
+    return valleysMoved;
+}
+
+
+console.log(countingValleys(8, 'UDDDUDUU'));
+console.log(countingValleys(10, 'DDUUDDUDUUUD'));

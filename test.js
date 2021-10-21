eb29a6d243c1074948f91c62e253f7d9311eaacf
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-22 00:45:54 +0100

    Refactored

--- test.js
+++
@@ -1,9 +0,0 @@
-let numbers = [20, 90, 10, 324, 9, 73];
-
-let smallest = Math.min(...numbers);
-
-console.log(`The smallest is ${ smallest }`);
-
-let index = numbers.indexOf(smallest);
-
-console.log(index);

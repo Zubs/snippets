f184fc67a1334bbe57360c829691e610d5ead864
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-23 18:40:13 +0100

    Small docs

--- php/caesars_cipher/index.php
+++ php/caesars_cipher/index.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Move every letter in the given string by 13 positions in the alphabets.
+ * @param string $input String to be transformed
+ * @return string
+ */
 $rot13 = fn (string $input) => str_rot13($input);
 
 echo $rot13("SERR PBQR PNZC");
